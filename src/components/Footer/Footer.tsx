e74bb6df234aebfd779c8fa1e33ed6522b5af6ab
Oscar Leo <oscarfolitse@gmail.com>
2023-12-01 18:01:51 +0100

    My commit

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,65 @@
+import { Container } from './styles'
+
+import reactIcon from '../../assets/react-icon.svg'
+import linkedin from '../../assets/linkedin.svg'
+import githubIcon from '../../assets/github.svg'
+import whatsapp from '../../assets/whatsapp.svg'
+import telegram from '../../assets/telegram.svg'
+import instagramIcon from '../../assets/instagram.svg'
+// import discordIcon from '../../assets/discord.png'
+
+export function Footer() {
+  return (
+    <Container className="footer">
+      <a href="https://vinayaksingh.in" className="logo">
+        <span>Oscvr</span>
+        <span>Leo</span>
+      </a>
+      <div>
+        <p>
+          This Website was made with <img src={reactIcon} alt="React" />
+          {/* <span>❤️</span> */}
+        </p>
+      </div>
+
+      <div className="social-media">
+        <a
+          href="https://www.linkedin.com/in/oscar-folitse-15a05625b/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <img src={linkedin} alt="Linkedin" />
+        </a>
+        <a
+          href="https://github.com/Oscarleo11"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <img src={githubIcon} alt="GitHub" />
+        </a>
+        <a
+          href="https://wa.me/qr/OW53UGL7D7AXF1"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <img src={whatsapp} alt="Whatsapp" />
+        </a>
+        <a
+          href=" https://web.telegram.org/a/#-1001233636375"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <img src={telegram} alt="telegram" />
+        </a>
+        <a
+          href="https://www.instagram.com/oscar_leo.99"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <img src={instagramIcon} alt="Instagram" />
+        </a>
+     
+      </div>
+    </Container>
+  )
+}
